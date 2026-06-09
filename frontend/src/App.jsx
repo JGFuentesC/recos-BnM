46492f173182b620cca5e6cc049c57196b912ab6
Andres <andreshabib10@gmail.com>
2026-06-08 18:53:54 -0600

    Integra FeedProvider y feed real en App

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,9 @@
 import { Navigate, Route, Routes } from 'react-router-dom'
 import ProtectedRoute from './components/ProtectedRoute'
-import FeedPlaceholder from './pages/FeedPlaceholder'
+import { FeedProvider } from './contexts/FeedContext'
 import Login from './pages/Login'
 import LibraryPlaceholder from './pages/LibraryPlaceholder'
+import Feed from './pages/Feed'
 import MockFeed from './pages/MockFeed'
 import Onboarding from './pages/Onboarding'
 import Register from './pages/Register'
@@ -10,38 +11,39 @@ import './App.css'
 
 function App() {
   return (
-    // TODO: envolver con <FeedProvider> cuando Juan Carlos entregue FeedContext.jsx
-    <Routes>
-      <Route path="/" element={<Navigate to="/login" replace />} />
-      <Route path="/login" element={<Login />} />
-      <Route path="/register" element={<Register />} />
-      <Route
-        path="/onboarding"
-        element={
-          <ProtectedRoute>
-            <Onboarding />
-          </ProtectedRoute>
-        }
-      />
-      <Route
-        path="/feed"
-        element={
-          <ProtectedRoute>
-            <FeedPlaceholder />
-          </ProtectedRoute>
-        }
-      />
-      <Route
-        path="/library"
-        element={
-          <ProtectedRoute>
-            <LibraryPlaceholder />
-          </ProtectedRoute>
-        }
-      />
-      <Route path="/mock-feed" element={<MockFeed />} />
-      <Route path="*" element={<Navigate to="/" replace />} />
-    </Routes>
+    <FeedProvider>
+      <Routes>
+        <Route path="/" element={<Navigate to="/login" replace />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route
+          path="/onboarding"
+          element={
+            <ProtectedRoute>
+              <Onboarding />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/feed"
+          element={
+            <ProtectedRoute>
+              <Feed />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/library"
+          element={
+            <ProtectedRoute>
+              <LibraryPlaceholder />
+            </ProtectedRoute>
+          }
+        />
+        <Route path="/mock-feed" element={<MockFeed />} />
+        <Route path="*" element={<Navigate to="/" replace />} />
+      </Routes>
+    </FeedProvider>
   )
 }
 
